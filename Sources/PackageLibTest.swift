4fc9fc72de64fe33d194b636d08bf0d2d051d3d7
张良 <zhangliang@hothuati.com>
2017-02-07 10:54:43 +0800

    Initial commit

---
+++ Sources/PackageLibTest.swift
@@ -0,0 +1,4 @@
+struct PackageLibTest {
+
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import PackageLibTestTests
+
+XCTMain([
+     testCase(PackageLibTestTests.allTests),
+])
